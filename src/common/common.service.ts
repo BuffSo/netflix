d3ec2ad7afc2e6cb11f187e0b204d9e3a0d9f8f6
Jake <buffso@naver.com>
2025-06-12 22:48:06 +0900

    feat: implement multi-column cursor pagination

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ObjectLiteral, SelectQueryBuilder } from 'typeorm';
 import { PagePaginationDto } from './dto/page-pagination.dto';
 import { CursorPaginationDto } from './dto/cursor-pagination.dto';
@@ -18,20 +18,103 @@ export class CommonService {
     qb.skip(skip);
   }
 
-  applyCursorPaginationParamsToQb<T extends ObjectLiteral>(
+  async applyCursorPaginationParamsToQb<T extends ObjectLiteral>(
     qb: SelectQueryBuilder<T>,
     dto: CursorPaginationDto,
   ) {
-    const { order, take, id } = dto;
+    const { cursor, take } = dto;
+    let { order } = dto;
 
-    if (id) {
-      const direction = order === 'ASC' ? '>' : '<';
+    if (cursor) {
+      const decodedCursor = Buffer.from(cursor, 'base64').toString('utf-8');
 
-      /// order -> ASC : movie.id > :id
-      qb.where(`${qb.alias}.id ${direction} :id`, { id });
+      /**
+       * {
+       *  values: {
+       *    id: 27
+       *  },
+       *  order: ['id_DESC']
+       * }
+       */
+
+      // 타입 단언 사용
+      const cursorObj = JSON.parse(decodedCursor) as {
+        values: Record<string, unknown>;
+        order: string[];
+      };
+
+      order = cursorObj.order;
+
+      const { values } = cursorObj;
+
+      /// (movie.column1, movie.column2, movie.column3) > (:value1, :value2, :value3)
+
+      // const comparisonOperator = order.some((o) => o.endsWith('DESC'))
+      //   ? '<'
+      //   : '>';
+      // 첫번째 정렬 기준의 방향으로 결정
+      const comparisonOperator = order[0].endsWith('DESC') ? '<' : '>';
+
+      const columns = Object.keys(values);
+      const whereConditions = columns.map((c) => `${qb.alias}.${c}`).join(',');
+      const whereParams = columns.map((c) => `:${c}`).join(',');
+
+      qb.where(
+        `(${whereConditions}) ${comparisonOperator} (${whereParams})`,
+        values,
+      );
     }
 
-    qb.orderBy(`${qb.alias}.id`, order);
+    // ["likeCount_DESC", "id_DESC"]
+    for (let i = 0; i < order.length; i++) {
+      const [column, direction] = order[i].split('_');
+
+      if (direction !== 'ASC' && direction !== 'DESC') {
+        throw new BadRequestException('Order는 ASC 또는 DESC로 입력해주세요~');
+      }
+
+      if (i === 0) {
+        qb.orderBy(`${qb.alias}.${column}`, direction);
+      } else {
+        qb.addOrderBy(`${qb.alias}.${column}`, direction);
+      }
+    }
+
     qb.take(take);
+
+    const results = await qb.getMany();
+
+    const nextCursor = this.generateNextCursor(results, order);
+
+    return { qb, nextCursor };
+  }
+
+  generateNextCursor<T>(results: T[], order: string[]): string | null {
+    if (results.length === 0) return null;
+
+    /**
+     * {
+     *  values: {
+     *    id: 27
+     *  },
+     *  order: ['id_DESC']
+     * }
+     */
+
+    const lastItem = results[results.length - 1];
+
+    const values: Record<string, unknown> = {};
+
+    order.forEach((columnOrder) => {
+      const [column] = columnOrder.split('_');
+      values[column] = lastItem[column];
+    });
+
+    const cursorObj = { values, order };
+    const nextCursor = Buffer.from(JSON.stringify(cursorObj)).toString(
+      'base64',
+    );
+
+    return nextCursor;
   }
 }
